cb705185bb56d25e3f8fee29ffd6e8182c4263cd
Yashwin Agrawal <yashwin18205@iiitd.ac.in>
2020-09-04 19:53:22 +0530

    first commit

---
+++ app/src/main/java/com/example/meme_creator/BottomPictureFragment.java
@@ -0,0 +1,30 @@
+package com.example.meme_creator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BottomPictureFragment extends Fragment{
+
+    private static TextView topMemeText;
+    private static TextView bottomMemeText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+        topMemeText = (TextView) view.findViewById(R.id.topMemeText);
+        bottomMemeText = (TextView) view.findViewById(R.id.bottomMemeText);
+        return view;
+    }
+
+    public void setMemeText(String top, String bottom){
+        topMemeText.setText(top);
+        bottomMemeText.setText(bottom);
+    }
+}
